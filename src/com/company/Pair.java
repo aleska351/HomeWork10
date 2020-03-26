ffa6c38485471036e544475b2347868bfe2b8ff8
Alena Goncharenko <51741689+aleska351@users.noreply.github.com>
2020-03-26 21:04:54 +0200

    HomeWork12 hillel school

---
+++ src/com/company/Pair.java
@@ -0,0 +1,25 @@
+package com.company;
+import java.util.Objects;
+
+public class Pair<K, V> {
+
+    public final K key;
+    public final V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public boolean equals(Object o) {
+        return o instanceof Pair && Objects.equals(key, ((Pair<?,?>)o).key) && Objects.equals(value, ((Pair<?,?>)o).value);
+    }
+
+    public int hashCode() {
+        return 31 * Objects.hashCode(key) + Objects.hashCode(value);
+    }
+
+    public String toString() {
+        return key + "=" + value;
+    }
+}
